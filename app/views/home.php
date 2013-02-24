71e2e918c2f660a416d992d4909d346bf8c0aa8f
Joël Cox <joel@joelcox.nl>
2013-02-24 16:08:14 +0100

    Implemented basic template.

---
+++ app/views/home.php
@@ -0,0 +1,277 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Codee - Een evenement voor web professionals</title>
+	<link rel="stylesheet" href="/css/general.css" type="text/css" media="screen" />
+
+	<script type="text/javascript" src="//use.typekit.net/vdz7cbf.js"></script>
+	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+</head>
+
+<body>
+
+	<header>
+		<div class="wrapper">
+
+			<header>
+				<img src="images/logo.png" alt="Codee logo" rel="logo" />
+				<span id="event-info">Zwolle, 14 april 2013</span>
+				<a href="#apply" id="sign-up" class="button">Schrijf je in</a>
+			</header>
+
+			<h1>
+				Codee is een kleinschalig evenement voor professionals
+				die zich met design en code bezig houden.
+			</h1>
+
+			<span class="more">Meer over Codee</span>
+
+		</div>
+	</header>
+
+	<section id="speakers">
+		<div class="wrapper">
+
+			<h1>Sprekers</h1>
+
+			<div class="speaker">
+
+				<div class="portrait">
+					<img src="/images/joel-cox.jpg" height="170" alt="Foto van Jo&euml; Cox" />
+				</div>
+
+				<h2>Milan van Bruggen</h2>
+
+				<p>Lalalala</p>
+
+				<span class="more">Meer over Milan</span>
+
+			</div>
+
+			<div class="speaker">
+
+				<div class="portrait">
+					<img src="/images/joel-cox.jpg" height="170" alt="Foto van Jo&euml; Cox" />
+				</div>
+
+				<h2>Milan van Bruggen</h2>
+
+				<p>Lalalala</p>
+
+				<span class="more">Meer over Milan</span>
+
+			</div>
+
+			<div class="speaker">
+
+				<div class="portrait">
+					<img src="/images/joel-cox.jpg" height="170" alt="Foto van Jo&euml; Cox" />
+				</div>
+
+				<h2>Milan van Bruggen</h2>
+
+				<p>Lalalala</p>
+
+				<span class="more">Meer over Milan</span>
+
+			</div>
+
+		</div>
+	</section>
+
+	<section id="attendees">
+		<div class="wrapper">
+
+			<h1>Deze pro's hebben zich al ingeschreven</h1>
+
+			<div class="attendee">
+
+				<div class="portrait">
+					<img src="/images/joel-cox.jpg" height="90" alt="Foto van Jo&euml; Cox" />
+				</div>
+
+				<h2>Jo&euml;l Cox</h1>
+
+				<h3>Developer</h3>
+				<h3>Skouti/Mickly</h2>
+
+			</div>
+
+			<div class="attendee">
+
+				<div class="portrait">
+					<img src="/images/joel-cox.jpg" height="90" alt="Foto van Jo&euml; Cox" />
+				</div>
+
+				<h2>Jo&euml;l Cox</h1>
+
+				<h3>Developer</h3>
+				<h3>Skouti/Mickly</h2>
+
+			</div>
+
+			<div class="attendee">
+
+				<div class="portrait">
+					<img src="/images/joel-cox.jpg" height="90" alt="Foto van Jo&euml; Cox" />
+				</div>
+
+				<h2>Jo&euml;l Cox</h1>
+
+				<h3>Developer</h3>
+				<h3>Skouti/Mickly</h2>
+
+			</div>
+
+			<div class="attendee">
+
+				<div class="portrait">
+					<img src="/images/joel-cox.jpg" height="90" alt="Foto van Jo&euml; Cox" />
+				</div>
+
+				<h2>Jo&euml;l Cox</h1>
+
+				<h3>Developer</h3>
+				<h3>Skouti/Mickly</h2>
+
+			</div>
+
+			<div class="attendee">
+
+				<div class="portrait">
+					<img src="/images/joel-cox.jpg" height="90" alt="Foto van Jo&euml; Cox" />
+				</div>
+
+				<h2>Jo&euml;l Cox</h1>
+
+				<h3>Developer</h3>
+				<h3>Skouti/Mickly</h2>
+
+			</div>
+
+			<div class="attendee">
+
+				<div class="portrait">
+					<img src="/images/joel-cox.jpg" height="90" alt="Foto van Jo&euml; Cox" />
+				</div>
+
+				<h2>Jo&euml;l Cox</h1>
+
+				<h3>Developer</h3>
+				<h3>Skouti/Mickly</h2>
+
+			</div>
+
+			<div class="attendee">
+
+				<div class="portrait">
+					<img src="/images/joel-cox.jpg" height="90" alt="Foto van Jo&euml; Cox" />
+				</div>
+
+				<h2>Jo&euml;l Cox</h1>
+
+				<h3>Developer</h3>
+				<h3>Skouti/Mickly</h2>
+
+			</div>
+
+			<div class="attendee">
+
+				<div class="portrait">
+					<img src="/images/joel-cox.jpg" height="90" alt="Foto van Jo&euml; Cox" />
+				</div>
+
+				<h2>Jo&euml;l Cox</h1>
+
+				<h3>Developer</h3>
+				<h3>Skouti/Mickly</h2>
+
+			</div>
+
+			<div class="attendee">
+
+				<div class="portrait">
+					<img src="/images/joel-cox.jpg" height="90" alt="Foto van Jo&euml; Cox" />
+				</div>
+
+				<h2>Jo&euml;l Cox</h1>
+
+				<h3>Developer</h3>
+				<h3>Skouti/Mickly</h2>
+
+			</div>
+
+			<div class="attendee">
+
+				<div class="portrait">
+					<img src="/images/joel-cox.jpg" height="90" alt="Foto van Jo&euml; Cox" />
+				</div>
+
+				<h2>Jo&euml;l Cox</h1>
+
+				<h3>Developer</h3>
+				<h3>Skouti/Mickly</h2>
+
+			</div>
+
+		</div>
+	</section>
+
+	<section id="availability">
+
+		<div class="wrapper">
+			<h2>Er zijn nog <span class="button">13</span> plaatsen vrij</h2>
+		</div>
+
+	</section>
+
+	<section id="apply">
+		<div class="wrapper">
+
+			<h1>Get invited</h1>
+
+			<p>
+				Wij kennen niet iedere goede programmeur of designer, maar
+				misschien ken jij iemand.
+			</p>
+
+			<form>
+				<input type="text" name="name" placeholder="Je naam" />
+				<select name="area_of_expertise">
+					<option>Je vakgebied</option>
+					<option>Designer</option>
+					<option>Frontend-er</option>
+					<option>Developer</option>
+				</select>
+				<input type="text" name="company" placeholder="Je bedrijf/werkgever" />
+
+				<input type="submit" name="submit" value="Schrijf je in" />
+			</form>
+
+		</div>
+	</section>
+
+	<footer>
+		<div class="wrapper">
+
+			<p>Mede mogelijk gemaakt door:</p>
+
+			<ul id="sponsors">
+				<li><a href="http://pixelpillow.nl"><img src="images/sponsors/pixelpillow.png" alt="Pixelpillow logo" /></a></li>
+				<li><a href="http://pixelpillow.nl"><img src="images/sponsors/pixelpillow.png" alt="Pixelpillow logo" /></a></li>
+				<li><a href="http://pixelpillow.nl"><img src="images/sponsors/pixelpillow.png" alt="Pixelpillow logo" /></a></li>
+			</ul>
+
+			<hr />
+
+			<p id="initiators">
+				Codee is een initatief van <a href="http://wildeweg.nl">Gertjan de Jong</a>,
+				<a href="http://milanvanbruggen.nl">Milan van Bruggen</a> en
+				<a href="http://joelcox">Jo&euml;l Cox</a>.
+			</p>
+
+		</div>
+	</div>
+
+
+</body>
